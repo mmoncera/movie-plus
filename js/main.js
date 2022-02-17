142129eef6e81d3f4b8495244b3db1cea1b3f669
Michael Moncera <michaelmoncera@gmail.com>
2022-02-17 23:38:48 +0000

    Added a sibling div[data-view="search-results"] to the HTML. #2

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
-var $form = document.querySelector('.form');
+var $formHome = document.querySelector('.form-home');
 
-$form.addEventListener('submit', handleSearch);
+$formHome.addEventListener('submit', handleSearch);
 
 function handleSearch(event) {
   event.preventDefault();
   data.searchResults = [];
-  data.searchInput = $form.elements.search.value;
+  data.searchInput = $formHome.elements.search.value;
   var searchQuery = new XMLHttpRequest();
   searchQuery.open(
     'GET',
     `http://www.omdbapi.com/?apikey=f1112d72&type=movie&s=${data.searchInput}`
   );
   searchQuery.responseType = 'json';
-  searchQuery.addEventListener('load', function () {
-    searchQuery.response.Search.forEach(({ imdbID, Poster, Title, Year }) => {
-      if (Poster !== 'N/A') {
-        var movie = {
-          imdbID,
-          Poster,
-          Title,
-          Year
-        };
-        data.searchResults.push(movie);
-      }
-    });
+  searchQuery.addEventListener('load', function (event) {
+    // console.log(searchQuery.response);
+    searchMovie(searchQuery.response.Search);
   });
   searchQuery.send();
+}
+
+function searchMovie(response) {
+  response.forEach(({ imdbID, Poster, Title, Year }) => {
+    if (Poster !== 'N/A') {
+      var movie = {
+        imdbID,
+        Poster,
+        Title,
+        Year
+      };
+      data.searchResults.push(movie);
+    }
+  });
 }
