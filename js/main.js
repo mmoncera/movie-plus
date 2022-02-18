002d9f3b6142b44ba481157395276b189c0c8e17
Michael Moncera <michaelmoncera@gmail.com>
2022-02-18 04:39:32 +0000

    Added a search bar and pre-populated it with the search input. #2

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ DOM Nodes
 var $appHome = document.querySelector('.app-home');
 var $dataView = document.querySelectorAll('[data-view]');
 var $formHome = document.querySelector('.form-home');
+var $formSearchResults = document.querySelector('.form-search-results');
+var $searchMessage = document.querySelector('.search-message');
 
 /*
 ************************************************
@@ -24,19 +26,23 @@ function switchDataView(view) {
   });
 }
 
-function searchMovie(response) {
-  response.forEach(({ imdbID, Poster, Title, Year }) => {
-    if (Poster !== 'N/A') {
-      var movie = {
-        imdbID,
-        Poster,
-        Title,
-        Year
-      };
-      data.searchResults.push(movie);
-    }
-  });
-}
+// function searchMovie(response) {
+//   if (!response) {
+//     $searchResultsMessage.textContent = 'Movie not found!';
+//   } else {
+//     response.forEach(({ imdbID, Poster, Title, Year }) => {
+//       if (Poster !== 'N/A') {
+//         var movie = {
+//           imdbID,
+//           Poster,
+//           Title,
+//           Year
+//         };
+//         data.searchResults.push(movie);
+//       }
+//     });
+//   }
+// }
 
 /*
 ************************************************
@@ -57,10 +63,24 @@ function handleSearch(event) {
     `http://www.omdbapi.com/?apikey=f1112d72&type=movie&s=${data.searchInput}`
   );
   searchQuery.responseType = 'json';
+
   searchQuery.addEventListener('load', function (event) {
     // console.log(searchQuery.response);
-    searchMovie(searchQuery.response.Search);
+    searchQuery.response.Search.forEach(({ imdbID, Poster, Title, Year }) => {
+      $formSearchResults.elements.search.value = data.searchInput;
+      $searchMessage.textContent = `Search results for "${data.searchInput}"`;
+      if (Poster !== 'N/A') {
+        var movie = {
+          imdbID,
+          Poster,
+          Title,
+          Year
+        };
+        data.searchResults.push(movie);
+      }
+    });
   });
+
   searchQuery.send();
   switchDataView('search-results');
 }
